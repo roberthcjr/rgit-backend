67e897a033a3a6341060fd51b6acfb04e2fc6d34
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-07 14:54:40 -0300

    feat(swagger): initializes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('RGIT')
+    .setDescription('The tools management system')
+    .setVersion('1.0')
+    .addTag('tools')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
   });
 
   await app.listen(process.env.PORT ?? 3000);
