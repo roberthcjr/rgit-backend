18df7e6a8f9ad0d9d5b59f87042a25436713fcfd
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-07 13:55:44 -0300

    feat: centralizes expiration time in .env

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: jwtConstants.expire },
     }),
   ],
   controllers: [AuthController],
--- backend/src/auth/constants/jwt-local.constants.ts
+++ backend/src/auth/constants/jwt-local.constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
   secret: process.env.JWT_SECRET,
+  expire: process.env.JWT_EXPIRE_TIME_IN_DAYS,
 };
